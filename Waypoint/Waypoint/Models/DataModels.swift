594e8ef7ee58ca5d7e9a9d8c289b8c7d5da40f9f
brentonford <brenton.ford@synerty.com>
2025-11-10 15:34:11 +1100

    feat: initialize iOS app project structure

---
+++ Waypoint/Waypoint/Models/DataModels.swift
@@ -0,0 +1,55 @@
+import Foundation
+import CoreLocation
+
+struct Waypoint: Identifiable, Codable {
+    let id: UUID
+    let coordinate: CLLocationCoordinate2D
+    var name: String
+    var comments: String
+    var photoData: Data?
+    var iconName: String
+    let createdDate: Date
+    var lastUpdatedDate: Date
+    
+    init(coordinate: CLLocationCoordinate2D, name: String = "Waypoint", comments: String = "", iconName: String = "mappin.circle.fill") {
+        self.id = UUID()
+        self.coordinate = coordinate
+        self.name = name
+        self.comments = comments
+        self.photoData = nil
+        self.iconName = iconName
+        self.createdDate = Date()
+        self.lastUpdatedDate = Date()
+    }
+}
+
+extension CLLocationCoordinate2D: Codable {
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(latitude, forKey: .latitude)
+        try container.encode(longitude, forKey: .longitude)
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let latitude = try container.decode(Double.self, forKey: .latitude)
+        let longitude = try container.decode(Double.self, forKey: .longitude)
+        self.init(latitude: latitude, longitude: longitude)
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case latitude
+        case longitude
+    }
+}
+
+struct DeviceStatus: Codable {
+    let hasGpsFix: Bool
+    let satellites: Int
+    let currentLat: Double
+    let currentLon: Double
+    let altitude: Double
+    let heading: Double
+    let distance: Double
+    let bearing: Double
+}
---
+++ Waypoint/Waypoint/WaypointApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaypointApp.swift
+//  Waypoint
+//
+//  Created by BRENTON FORD on 9/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct WaypointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
